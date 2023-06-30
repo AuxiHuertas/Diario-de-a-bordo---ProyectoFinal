e6baba0fbbd15cb0686d860d3ff0f9eb30ab8aca
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-06-30 20:33:03 +0200

    fix:signup controller, email value of body

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -2,11 +2,13 @@ const errors = require('../../misc/errors')
 
 module.exports = (db) => async (req,res,next) =>  {
 
-    const {username , password}= req.body;
+    const {username , password, email }= req.body;
 
-    console.info(username,password)
+    console.info(username,password, email )
 
-    if(!username || !password) return next(errors[400])
+    if(!username || !password) return next (errors[400])
+
+
 
     res.status(200).json({
         success: true,
