0a14f5d1ce948d8f9425fd3fc87af572b058d240
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-06-30 19:37:31 +0200

    feat:(back) controllador signup, solicitud http y control de datos null

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,5 +1,14 @@
+const errors = require('../../misc/errors')
+
 module.exports = (db) => async (req,res,next) =>  {
+
+    const {username , password}= req.body;
+
+    console.info(username,password)
+
+    if(!username || !password) return next(errors[400])
+
     res.status(200).json({
-        succcess: true,
+        success: true,
     })
 }
