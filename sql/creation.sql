127dec10f7d1f699f14488da7b5a274ddca09598
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-06-30 18:50:21 +0200

    fix(back):new position of the coments in tables

--- sql/creation.sql
+++ sql/creation.sql
@@ -6,28 +6,28 @@ CREATE TABLE IF NOT EXISTS users (
     password TEXT NOT NULL,
     email TEXT NOT NULL 
 );
---latlng_country Este campo vendrá volcado del mapa interactivo de leaflet
---name_country Para que este dato aparezca, debes de volcar el campo latlng en geonames.org y traer el nombre de aquí
+
+
 
  CREATE TABLE IF NOT EXISTS country (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     id_user uuid REFERENCES users NOT NULL,
-    name_country TEXT NOT NULL,
-    latlng_country TEXT NOT NULL
+    name_country TEXT NOT NULL -- Este campo vendrá volcado del mapa interactivo de leaflet
+    latlng_country TEXT NOT NULL -- Para que este dato aparezca, debes de volcar el campo latlng en geonames.org y traer el nombre de aquí
 );
---date_activity este campo debe ser un calendario
---hour_activity este campo debe ser un reloj 
+
 
 CREATE TABLE IF NOT EXISTS activities (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     id_country uuid REFERENCES country NOT NULL,
     name_activity TEXT NOT NULL,
-    date_activity DATE, 
-    hour_activity TIME
+    date_activity DATE -- este campo debe ser un calendario
+    hour_activity TIME -- este campo debe ser un reloj 
+
 );
 
 CREATE TABLE IF NOT EXISTS img (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     id_country uuid REFERENCES country NOT NULL,
-    img VARCHAR(200) -- img Aquí debe llegar mediante llamada POST la URL de la imagen a través de Cloudinar
+    img VARCHAR(200) -- Aquí debe llegar mediante llamada POST la URL de la imagen a través de Cloudinar
 );
