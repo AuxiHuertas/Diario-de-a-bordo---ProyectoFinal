e02bd2591b428e8e0f6485cb53b5e407f0ddac34
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-07-02 14:29:00 +0200

    feat(back): Singin , controller and models, validation, compare and user access

--- models/auth/index.js
+++ models/auth/index.js
@@ -1,5 +1,6 @@
-const e = require("express")
-const { insertUser } = require("./queries")
+
+const { insertUser, selectUser } = require("./queries")
+
 
 const createNewUser = (db) => async (username,password,email) => {
     try{
@@ -18,6 +19,35 @@ const createNewUser = (db) => async (username,password,email) => {
     }
 }
 
-module.export = {
+const selectByUsername = (db) => async (username,compareFn) => {
+    try{
+        const user = await db.maybeOne(selectUser(username)) // En la query selectUser seleccionamos username y password, (la password aqui está encriptada)
+        console.log(">password encriptada", user)
+
+        if(!user) return {
+            ok: false,
+            error_code : 'wrong_data'
+        }
+
+        const areEqual = await compareFn(user.password) //El password encriptado lo está tomando de la query, por ello utiliza user. 
+
+        console.log("Son iguales?", areEqual)
+
+        return {
+            ok :true
+        }
+    }
+    
+    catch (error){
+        console.info(">create new user error", error.message)
+        return {
+            ok:false,
+            message: error.message,
+        }
+    }
+}
+
+module.exports = {
     createNewUser,
+    selectByUsername,
 }
--- models/auth/queries.js
+++ models/auth/queries.js
@@ -1,12 +1,22 @@
 const { sql } = require('slonik')
 
-const insertUser = () => sql.unsafe `
+
+const insertUser = (username,password,email) => sql.unsafe `
 INSERT INTO users (
     username, password, email
 ) VALUES (
     ${username},${password},${email}
 )
 `
+
+const selectUser = (username) => sql.unsafe `
+
+    SELECT username, password
+    FROM users
+    WHERE username LIKE ${username};
+
+`
 module.exports = {
     insertUser,
+    selectUser,
 }
