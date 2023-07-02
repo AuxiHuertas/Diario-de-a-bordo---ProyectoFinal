e70d6c740898079ef249f9a9f4a373687c3907b3
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-07-02 14:30:51 +0200

    fix: bug comment localized and fixed

--- sql/creation.sql
+++ sql/creation.sql
@@ -6,15 +6,16 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS users (
     id uuid PRIMARY KEY DEFAULT  uuid_generate_v4(),
-    username TEXT NOT NULL UNIQUE,
+    username TEXT NOT NULL UNIQUE,-- Está creando valores duplicados a pesar de ser UNIQUE , 
     password TEXT NOT NULL,
     email TEXT NOT NULL 
 );
 
+
  CREATE TABLE IF NOT EXISTS country (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     id_user uuid REFERENCES users NOT NULL,
-    name_country TEXT NOT NULL -- Este campo vendrá volcado del mapa interactivo de leaflet
+    name_country TEXT NOT NULL, -- Este campo vendrá volcado del mapa interactivo de leaflet
     latlng_country TEXT NOT NULL -- Para que este dato aparezca, debes de volcar el campo latlng en geonames.org y traer el nombre de aquí
 );
 
@@ -23,7 +24,7 @@ CREATE TABLE IF NOT EXISTS activities (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     id_country uuid REFERENCES country NOT NULL,
     name_activity TEXT NOT NULL,
-    date_activity DATE -- este campo debe ser un calendario
+    date_activity DATE, -- este campo debe ser un calendario
     hour_activity TIME -- este campo debe ser un reloj 
 
 );
