598b73052aa1fa2353f71f8d761cee362d475bc3
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-07-04 20:24:30 +0200

    feat(back): fieldsValidator

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,11 +1,15 @@
 const router = require('express').Router()
-const { authorizer } = require('../middelwares')
+const { authorizer,fieldsValidator } = require('../middelwares')
 
 const authControllers = require('../controllers/auth')
 
+const signupValidator = fieldsValidator('email', 'username', 'password')
+const signinValidator = fieldsValidator('email', 'password')
+
+
 module.exports = (db) => {
-    router.post('/signup', authControllers.signup(db))
-    router.post('/signin', authControllers.signin(db))
+    router.post('/signup',fieldsValidator, authControllers.signup(db))
+    router.post('/signin',fieldsValidator, authControllers.signin(db))
     router.post('/signout',authorizer,  authControllers.signout(db))
 
     return router
