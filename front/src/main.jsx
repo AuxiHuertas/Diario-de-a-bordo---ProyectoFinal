76e59d912905cc4a1026fb0848123989f97eca6a
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-07-05 21:03:10 +0200

    feat(front): jsx file for panel and login, installation of wounter and configure the routes of a React application using wounter

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Login from './pages/login';
+import Panel from './pages/panel';
+import {Swicht, Route, Redirect} from 'wouter'
+
+
+const Main = () => {
+  return (
+    <Swicht>
+      <Route path="/login" component={Login}/>
+      <Route path="/" component={Panel} />
+      <Redirect to = "/login" />
+    </Swicht>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
 )
---
+++ front/src/pages/panel/index.jsx
@@ -0,0 +1,10 @@
+const Panel = () => {
+    return (
+        <section>
+            <h1>Login Page</h1>
+            <p>(Unprotected route) </p>
+        </section>
+    );
+};
+
+export default Panel
