41edaec5f78d8a40936a04b1c9f59feead81a333
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-07-06 21:03:04 +0200

    feat(front): import and apply registration function and validations, and handleSubmit

--- front/src/pages/login/index.jsx
+++ front/src/pages/login/index.jsx
@@ -1,25 +1,27 @@
-
-
-
+import {useForm} from 'react-hook-form'
 
 const Login = () => {
- 
+    const {register, formState, handleSubmit} = useForm()
+
+    const handleForm = (data) => {
+
+        console.info(">form data", data);
+    };
 
     return (
         <section>
             <h1>Login Page</h1>
             <p>(Protected route) </p>
-            <form>
-                <label htmlFor="email">email</label><br/>
-                <input id="email" placeholder="user@user.com"></input>
+            <form onSubmit={handleSubmit(handleForm)}>
+                <label htmlFor="email">emails</label><br/>
+                <input id="email" placeholder="user@user.com"{...register("email",{ required: true})}></input>
                 <p></p>
 
-                <label htmlFor="username">email</label><br/>
-                <input id="username" placeholder=" "></input>
+                <label htmlFor="username">username</label><br/>
+                <input id="username" placeholder="user" {...register("username",{ required: true})}></input>
                 <p></p>
-
-                <label htmlFor="password">email</label><br/>
-                <input id="password" type="password" placeholder="******"></input>
+                <label htmlFor="password">password</label><br/>
+                <input id="password" type="password" placeholder="******"{...register("password",{ required: true, minLength: 4})} ></input>
                 <p></p>
 
                 <input type="submit"/>
