7a00dc835dba2ad16dc05edd818bcbe751d51c8b
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-07-06 20:42:11 +0200

    feat(front):login forms

--- front/src/pages/login/index.jsx
+++ front/src/pages/login/index.jsx
@@ -1,8 +1,29 @@
+
+
+
+
 const Login = () => {
+ 
+
     return (
         <section>
             <h1>Login Page</h1>
             <p>(Protected route) </p>
+            <form>
+                <label htmlFor="email">email</label><br/>
+                <input id="email" placeholder="user@user.com"></input>
+                <p></p>
+
+                <label htmlFor="username">email</label><br/>
+                <input id="username" placeholder=" "></input>
+                <p></p>
+
+                <label htmlFor="password">email</label><br/>
+                <input id="password" type="password" placeholder="******"></input>
+                <p></p>
+
+                <input type="submit"/>
+            </form>
         </section>
     );
 };
