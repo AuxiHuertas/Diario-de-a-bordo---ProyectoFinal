7867863cb1078699857ed0f8d7c12eefb01631db
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-07-06 21:52:19 +0200

    feat(front): error handling from the formState function, creating general error schematics, and citing the error in <p></p>

--- front/src/pages/login/index.jsx
+++ front/src/pages/login/index.jsx
@@ -1,33 +1,78 @@
-import {useForm} from 'react-hook-form'
+import { useForm } from "react-hook-form";
 
-const Login = () => {
-    const {register, formState, handleSubmit} = useForm()
-
-    const handleForm = (data) => {
-
-        console.info(">form data", data);
-    };
-
-    return (
-        <section>
-            <h1>Login Page</h1>
-            <p>(Protected route) </p>
-            <form onSubmit={handleSubmit(handleForm)}>
-                <label htmlFor="email">emails</label><br/>
-                <input id="email" placeholder="user@user.com"{...register("email",{ required: true})}></input>
-                <p></p>
-
-                <label htmlFor="username">username</label><br/>
-                <input id="username" placeholder="user" {...register("username",{ required: true})}></input>
-                <p></p>
-                <label htmlFor="password">password</label><br/>
-                <input id="password" type="password" placeholder="******"{...register("password",{ required: true, minLength: 4})} ></input>
-                <p></p>
-
-                <input type="submit"/>
-            </form>
-        </section>
-    );
+const template = {
+  login: {
+    email: {
+      validation: {
+        required: true,
+      },
+    },
+    username: {
+      validation: {
+        required: true,
+      },
+    },
+    password: {
+      validation: {
+        required: true,
+        minLength: 4,
+      },
+    },
+    errors: {
+      required: "This field is mandatory",
+      minLength: "4 length is required",
+    },
+  },
 };
 
-export default Login
+const Login = () => {
+  const { register, formState, handleSubmit } = useForm();
+
+  const handleForm = (data) => {
+    console.info(">form data", data);
+  };
+
+  console.info("> form sate:", formState);
+
+  const { errors } = template.login
+
+  return (
+    <section>
+      <h1>Login Page</h1>
+      <p>(Protected route) </p>
+      <form onSubmit={handleSubmit(handleForm)}>
+        <label htmlFor="email">emails</label>
+        <br />
+        <input
+          id="email"
+          placeholder="user@user.com"
+          {...register("email", { required: true })}
+        ></input>
+        <p>{formState.errors && errors[formState.errors?.email?.type]}</p>
+
+        <label htmlFor="username">username</label>
+        <br />
+        <input
+          id="username"
+          placeholder="user"
+          {...register("username", { required: true })}
+        ></input>
+        <p>{formState.errors && errors[formState.errors?.username?.type]}</p>
+
+        <label htmlFor="password">password</label>
+        <br />
+        <input
+          id="password"
+          type="password"
+          placeholder="******"
+          {...register("password", { required: true, minLength: 4 })}
+        ></input>
+        <p>{formState.errors && errors[formState.errors?.password?.type]}</p>
+
+        <input type="submit" />
+      </form>
+    </section>
+  );
+};
+
+export default Login;
