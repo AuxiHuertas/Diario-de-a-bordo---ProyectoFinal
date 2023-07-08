1ddbf1c8743a3c8fbf752db9b644ac6551dce9b7
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-07-08 11:50:29 +0200

    feat(front): login with react-hook-forms and templates structure (templates) to define the properties and validations of the form fields

--- front/src/pages/login/index.jsx
+++ front/src/pages/login/index.jsx
@@ -1,29 +1,6 @@
 import { useForm } from "react-hook-form";
+import * as  templates  from "../../misc/templates.js";
 
-const template = {
-  login: {
-    email: {
-      validation: {
-        required: true,
-      },
-    },
-    username: {
-      validation: {
-        required: true,
-      },
-    },
-    password: {
-      validation: {
-        required: true,
-        minLength: 4,
-      },
-    },
-    errors: {
-      required: "This field is mandatory",
-      minLength: "4 length is required",
-    },
-  },
-};
 
 const Login = () => {
   const { register, formState, handleSubmit } = useForm();
@@ -34,7 +11,7 @@ const Login = () => {
 
   console.info("> form sate:", formState);
 
-  const { errors } = template.login
+  const { errors,email, username, password } = templates.login
 
   return (
     <section>
@@ -44,28 +21,25 @@ const Login = () => {
         <label htmlFor="email">emails</label>
         <br />
         <input
-          id="email"
-          placeholder="user@user.com"
-          {...register("email", { required: true })}
+
+          {...{...email.props,...register("email", email.validation)}}
+         
         ></input>
         <p>{formState.errors && errors[formState.errors?.email?.type]}</p>
 
         <label htmlFor="username">username</label>
         <br />
         <input
-          id="username"
-          placeholder="user"
-          {...register("username", { required: true })}
+
+          {...{...username.props,...register("username", username.validation),}}
         ></input>
         <p>{formState.errors && errors[formState.errors?.username?.type]}</p>
 
         <label htmlFor="password">password</label>
         <br />
         <input
-          id="password"
-          type="password"
-          placeholder="******"
-          {...register("password", { required: true, minLength: 4 })}
+
+          {...{...password.props,...register("password", password.validation)}}
         ></input>
         <p>{formState.errors && errors[formState.errors?.password?.type]}</p>
 
