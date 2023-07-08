92775ea1aaa6eaf8ab048f6a358b22e672b6143e
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-07-08 12:07:58 +0200

    feat(front) services

---
+++ front/src/services/user.js
@@ -0,0 +1,17 @@
+export const info = (client) => async () => {
+
+    try {
+
+      const { data: response } = await client.get("/users");
+      
+      console.info("> info data: ", response);
+
+      return response.data;
+
+    } catch (error) {
+
+      console.info("> info error: ", error.message);
+
+      return null;
+    }
+  };
