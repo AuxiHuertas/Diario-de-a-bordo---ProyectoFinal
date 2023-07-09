2db9588914e3033565abc98b068e5d3bab932d06
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-07-09 11:15:59 +0200

    response in case of error

--- front/src/services/auth.js
+++ front/src/services/auth.js
@@ -1,15 +1,19 @@
-const login = (client) => async (params) => {
+export const login = (client) => async (params) => {
 
     try{
 
         const {data} = await client.post('/auth/signin', params)
-        console.info('> login data', data)
+        console.info('> login data', data);
+
+        return data
         
 
     } catch (error) {
 
-        console.info ('> login error', error.message)
+        console.info ('> login error', error.message);
+        return { success: false};
 
     }
 
 }
+
