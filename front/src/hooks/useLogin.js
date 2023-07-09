1f71d655853321c60369ca076a3508128cc3cff8
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-07-09 11:32:04 +0200

    feat(front) New hook folder to contain the useLogin variable that was previously in page/login/index Import of the useQueryclient and useMutate hooks from React-query and import of auth from the services file, for use in useLogin

---
+++ front/src/hooks/useLogin.js
@@ -0,0 +1,19 @@
+
+import {useMutation, useQueryClient} from 'react-query';
+import { auth } from '../services';
+
+
+export const useLogin  = () => {
+    const queryClient = useQueryClient()
+  
+    const { mutate } = useMutation({
+    
+        mutationFn: auth.login,
+        onSuccess: (response) => {
+            console.info("mutation response",response)
+            if (response.success) 
+                queryClient.invalidateQueries({ queryKey : ["user"]});
+        }
+    });
+    return mutate
+  }
