7453bffa9391894e3c932318d355cce4ec7ef407
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-07-12 19:22:27 +0200

    feat(back) market route for map marker

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,10 +1,12 @@
 const router = require ('express').Router()
 const authRoutes = require ('./auth')
 const userRoutes = require ('./users')
+const marketRoutes = require ('./market')
 
 module.exports = (db) => {
     router.use('/auth', authRoutes(db))
     router.use('/users', userRoutes())
+    router.use('/market', marketRoutes(db))
 
     return router
 }
